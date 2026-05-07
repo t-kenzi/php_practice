2814b0417b64cedcd2a418d250630348d3b7a43e
kenzi1111 <88967765+kenzi1111@users.noreply.github.com>
2026-05-07 21:28:45 +0900

    change LibraryCatalog.php

--- src/parc20260429/LibraryCatalog.php
+++ src/parc20260429/LibraryCatalog.php
@@ -1,9 +1,10 @@
 <?php
+include "php_base/src/parc20260429/Book.php";
 
 class LibraryCatalog
 {
     // 保持する情報
-    // - 本一覧
+    // - 本一覧：オブジェクトを作成
 
     // 持つべき機能
     // - 本を登録する
@@ -17,5 +18,18 @@ class LibraryCatalog
     // - 本IDから Book オブジェクトを取得する
     // - 本が存在するか確認する
     // - 貸出処理そのものは担当しない
+
+
+    private array $booklists;
+
+    public function __construct()
+    {
+        $this->booklists = [];
+    }
+
+    public function registerBook(int $id,string $title,$lendStatus = false){
+        $newBook = new Book();
+
+    }
 }
 
